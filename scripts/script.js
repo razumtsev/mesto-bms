65b235d1e3d1809bd191442b543bdf39988332d1
Ilya Razumtsev <razumtsev.il@yandex.ru>
2024-09-26 15:09:19 +0300

    initial cards from array

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,33 @@
+import { initialCards } from "./initialCards.js";
+
 const page = document.querySelector('.page');
 const buttonEditProfile = page.querySelector('.profile__edit');
 const profileUsername = page.querySelector('.profile__username');
 const profileAbout = page.querySelector('.profile__about');
 const popup = page.querySelector('.popup');
 const popupEditProfile = page.querySelector('.popup_type_edit-profile');
-const formInputUsername = popupEditProfile.querySelector('.form__input_type_username');
-const formInputAbout = popupEditProfile.querySelector('.form__input_type_about');
-const buttonFormSubmit = popupEditProfile.querySelector('.form__submit');
+const formTypeEditProfile = popupEditProfile.querySelector('.form_type_edit-profile');
+const formInputUsername = formTypeEditProfile.querySelector('.form__input_type_username');
+const formInputAbout = formTypeEditProfile.querySelector('.form__input_type_about');
 const buttonClosePopup = popup.querySelector('.popup__close');
+
+const cardsList = document.querySelector('.cards__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+const createCard = ({ link, name }) => {
+  const card = cardTemplate.cloneNode(true);
+  const cardImage = card.querySelector('.card__image');
+  const cardTitle = card.querySelector('.card__title');
+  cardImage.src = link;
+  cardImage.alt = name;
+  cardTitle.textContent = name;
+  return card;
+}
+
+const renderCardAppend = (card) => cardsList.append(card);
+
+initialCards.forEach((item) => renderCardAppend(createCard(item)));
+
 
 const openPopup = (popupName) => popupName.classList.add('popup_is-open');
 const closePopup = (evt) => {
@@ -28,5 +48,5 @@ const handleFormSubmit = (evt) => {
 }
 
 buttonEditProfile.addEventListener('click', handleEditProfileButtonClick);
-buttonFormSubmit.addEventListener('click', handleFormSubmit);
+formTypeEditProfile.addEventListener('submit', handleFormSubmit);
 buttonClosePopup.addEventListener('click', closePopup);
