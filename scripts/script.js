13277dd8b63b38e9ae30f546e3caf861f01c63db
Ilya Razumtsev <razumtsev.il@yandex.ru>
2024-09-27 18:18:36 +0300

    popup Add Card is done

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,19 @@ import { initialCards } from "./initialCards.js";
 
 const page = document.querySelector('.page');
 const buttonEditProfile = page.querySelector('.profile__edit');
+const buttonAddCard = page.querySelector('.profile__add-card');
 const profileUsername = page.querySelector('.profile__username');
 const profileAbout = page.querySelector('.profile__about');
 const popup = page.querySelector('.popup');
 const popupEditProfile = page.querySelector('.popup_type_edit-profile');
 const formTypeEditProfile = popupEditProfile.querySelector('.form_type_edit-profile');
-const formInputUsername = formTypeEditProfile.querySelector('.form__input_type_username');
-const formInputAbout = formTypeEditProfile.querySelector('.form__input_type_about');
-const buttonClosePopup = popup.querySelector('.popup__close');
+const inputUsername = formTypeEditProfile.querySelector('.form__input_type_username');
+const inputAbout = formTypeEditProfile.querySelector('.form__input_type_about');
+const popupAddCard = page.querySelector('.popup_type_add-card');
+const formTypeAddCard = popupAddCard.querySelector('.form_type_add-card');
+const inputCardName = popupAddCard.querySelector('.form__input_type_name');
+const inputCardLink = popupAddCard.querySelector('.form__input_type_link');
+const buttonClosePopupCollection = document.querySelectorAll('.popup__close');
 
 const cardsList = document.querySelector('.cards__list');
 const cardTemplate = document.querySelector('#card-template').content;
@@ -25,6 +30,7 @@ const createCard = ({ link, name }) => {
 }
 
 const renderCardAppend = (card) => cardsList.append(card);
+const renderCardPrepend = (card) => cardsList.prepend(card);
 
 initialCards.forEach((item) => renderCardAppend(createCard(item)));
 
@@ -35,18 +41,36 @@ const closePopup = (evt) => {
 }
 
 const handleEditProfileButtonClick = () => {
-  formInputUsername.value = profileUsername.textContent;
-  formInputAbout.value = profileAbout.textContent;
+  inputUsername.value = profileUsername.textContent;
+  inputAbout.value = profileAbout.textContent;
   openPopup(popupEditProfile);
 }
 
-const handleFormSubmit = (evt) => {
+const handleAddCardButtonClick = () => {
+  openPopup(popupAddCard);
+}
+
+const handleEditProfileFormSubmit = (evt) => {
   evt.preventDefault();
-  profileUsername.textContent = formInputUsername.value;
-  profileAbout.textContent = formInputAbout.value;
+  profileUsername.textContent = inputUsername.value;
+  profileAbout.textContent = inputAbout.value;
   closePopup(evt);
 }
 
+const handleAddCardFormSubmit = (evt) => {
+  evt.preventDefault();
+  const card = {};
+  card.name = inputCardName.value;
+  card.link = inputCardLink.value;
+  renderCardPrepend(createCard(card));
+  closePopup(evt);
+  formTypeAddCard.reset();
+}
+
+console.log(buttonClosePopupCollection);
+
 buttonEditProfile.addEventListener('click', handleEditProfileButtonClick);
-formTypeEditProfile.addEventListener('submit', handleFormSubmit);
-buttonClosePopup.addEventListener('click', closePopup);
+buttonAddCard.addEventListener('click', handleAddCardButtonClick);
+formTypeEditProfile.addEventListener('submit', handleEditProfileFormSubmit);
+formTypeAddCard.addEventListener('submit', handleAddCardFormSubmit);
+buttonClosePopupCollection.forEach((item) => item.addEventListener('click', closePopup));
